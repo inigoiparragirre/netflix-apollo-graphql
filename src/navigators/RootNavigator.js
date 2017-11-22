57527da45e64a15463692dcaba17d95dbf15c20a
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2017-11-22 10:11:39 +0100

    Add navigator

--- src/navigators/RootNavigator.js
+++ src/navigators/RootNavigator.js
@@ -2,20 +2,40 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addNavigationHelpers, StackNavigator, DrawerNavigator } from 'react-navigation';
+import { addNavigationHelpers, TabNavigator } from 'react-navigation';
 
 import Dashboard from "../components/Home";
 import CategoryScreen from "../category";
 import Search from "../components/Search";
 
 
-export const RootNavigator = DrawerNavigator({
-  Dashboard: { screen: Dashboard },
-  CategoryScreen: { screen: CategoryScreen},
-  Search: { screen: Search}
-},
-{initialRouteName: 'Dashboard'}
-);
+export const RootNavigator = TabNavigator({
+  Dashboard: { 
+    screen: Dashboard ,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+    },
+  },
+  CategoryScreen: { 
+    screen: CategoryScreen,
+    navigationOptions: {
+      tabBarLabel: 'Category',
+    },
+  },
+  Search: { 
+    screen: Search,
+    navigationOptions: {
+      tabBarLabel: 'Search',
+    }
+  }
+}, {
+  tabBarPosition: 'top',
+  animationEnabled: true,
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
+  },
+});
+
 
 const AppWithNavigationState = ({ dispatch, nav }) => (
   <RootNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
